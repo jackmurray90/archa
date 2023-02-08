ae9e4dfc05672ca892304bdb426d9bd9b76fd817
Jack Murray <jack@murray.software>
2023-02-08 17:48:15 +1100

    comment the custom router code

--- archa/router.py
+++ archa/router.py
@@ -1,19 +1,23 @@
+'''
+This file enables us to use regular views in the router alongside viewsets.
+'''
+
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework.viewsets import ViewSetMixin
 from rest_framework.urlpatterns import format_suffix_patterns
 
 class ArchaRouter(DefaultRouter):
-    def get_routes(self, viewset):
-        if issubclass(viewset, ViewSetMixin):
-            return super(ArchaRouter, self).get_routes(viewset)
+    def get_routes(self, view):
+        if issubclass(view, ViewSetMixin):
+            return super(ArchaRouter, self).get_routes(view)
         return []
 
     def get_urls(self):
         urls = []
-        for prefix, viewset, basename in self.registry:
-            if issubclass(viewset, ViewSetMixin):
+        for prefix, view, basename in self.registry:
+            if issubclass(view, ViewSetMixin):
                 continue
-            urls.append(path(f'{prefix}{self.trailing_slash}', viewset.as_view(), name=f'{basename}-list'))
+            urls.append(path(f'{prefix}{self.trailing_slash}', view.as_view(), name=f'{basename}-list'))
         urls = format_suffix_patterns(urls, allowed=['json', 'html'])
         return super(ArchaRouter, self).get_urls() + urls
